6418ae868d51809fd28dbf65204a05db33d18e7e
prabutdr <prabutdr@prabutdr>
2021-05-06 18:39:47 +0530

    Array concates

--- study/src/main/java/com/prabutdr/study/arrays/ArrayConcate.java
+++ study/src/main/java/com/prabutdr/study/arrays/ArrayConcate.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ArrayConcate {
 
@@ -23,4 +24,34 @@ public class ArrayConcate {
 		System.arraycopy(array2, 0, resultArray, array1.length, array2.length);
 		return resultArray;
 	}
+	
+	public static <T> T concateForPrimitives(T array1, T array2) {
+		if (!array1.getClass().isArray() || !array2.getClass().isArray()) {
+			throw new IllegalArgumentException("Only arrays are accepted");
+		}
+		
+		Class<?> compType1 = array1.getClass().getComponentType();
+		Class<?> compType2 = array2.getClass().getComponentType();
+		if (!compType1.equals(compType2)) {
+			throw new IllegalArgumentException("Two arrays have different types");
+		}
+		
+		int len1 = Array.getLength(array1);
+		int len2 = Array.getLength(array2);
+		
+		@SuppressWarnings("unchecked")
+		T result = (T) Array.newInstance(compType2, len1 + len2);
+		
+		System.arraycopy(array1, 0, result, 0, len1);
+		System.arraycopy(array2, 0, result, len1, len2);
+		
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T[] concateUsingStream(T[] array1, T[] array2) {
+		return Stream.concat(Arrays.stream(array1), Arrays.stream(array2))
+			.toArray(size -> (T[]) Array.newInstance(array1.getClass().getComponentType(), size));
+	}
+	
 }
--- study/src/test/java/com/prabutdr/study/arrays/ArrayConcateTests.java
+++ study/src/test/java/com/prabutdr/study/arrays/ArrayConcateTests.java
@@ -1,6 +1,7 @@
 package com.prabutdr.study.arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.stream.Stream;
 
@@ -34,6 +35,44 @@ public class ArrayConcateTests {
 	}
 	
 	public static Stream<Arguments> concateShouldCombineArraysUsingArrayCopy() {
+		return concateShouldCombineArraysUsingCollections();
+	}
+	
+	@ParameterizedTest
+	@MethodSource
+	public void concateForPrimitives(int[] array1, int[] array2, int[] expectedArray) {
+		int[] result = ArrayConcate.concateForPrimitives(array1, array2);
+		assertThat(result).isEqualTo(expectedArray);
+	}
+	
+	public static Stream<Arguments> concateForPrimitives() {
+		return Stream.of(
+				Arguments.of(new int[] {3}, new int[] {4}, new int[] {3, 4}),
+				Arguments.of(new int[] {}, new int[] {}, new int[] {})
+			);
+	}
+	
+	@ParameterizedTest
+	@MethodSource
+	public void concateForPrimitivesWithInvalidInputs(Object array1, Object array2) {
+		assertThrows(IllegalArgumentException.class, () -> ArrayConcate.concateForPrimitives(array1, array2));
+	}
+	
+	public static Stream<Arguments> concateForPrimitivesWithInvalidInputs() {
+		return Stream.of(
+				Arguments.of("abc", new int[] {4}, new int[] {3, 4}),
+				Arguments.of(new int[] {}, new float[] {}, new int[] {})
+			);
+	}
+	
+	@ParameterizedTest
+	@MethodSource
+	public void concateShouldCombineArraysUsingStream(Integer[] array1, Integer[] array2, Integer[] expectedArray) {
+		Integer[] result = ArrayConcate.concateUsingArrayCopy(array1, array2);
+		assertThat(result).isEqualTo(expectedArray);
+	}
+	
+	public static Stream<Arguments> concateShouldCombineArraysUsingStream() {
 		return concateShouldCombineArraysUsingCollections();
 	}
 	
